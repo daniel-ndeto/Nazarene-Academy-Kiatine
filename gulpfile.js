56ffb97d3c06e9969e3010060b8304006e39c7cd
daniel-ndeto <danielndeto222@gmail.com>
2025-10-08 20:19:29 +0300

    Implement code changes to enhance functionality and improve performance

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,36 @@
-/* Needed gulp config */
-var gulp = require("gulp");
-var gulpSass = require("gulp-sass");
-var sassCompiler = require("sass");
-var sass = gulpSass(sassCompiler);
-var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
-var notify = require("gulp-notify");
-var minifycss = require("gulp-minify-css");
-var concat = require("gulp-concat");
-var plumber = require("gulp-plumber");
-var browserSync = require("browser-sync");
-var reload = browserSync.reload;
+/* ===============================
+   Gulp Configuration
+================================= */
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const notify = require("gulp-notify");
+const minifycss = require("gulp-minify-css");
+const concat = require("gulp-concat");
+const plumber = require("gulp-plumber");
+const browserSync = require("browser-sync").create();
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer").default;
 
-/* Setup scss path */
-var paths = {
-  scss: "./sass/*.scss",
-};
-
-/* Scripts task */
-gulp.task("scripts", function () {
-  return gulp
-    .src([
-      /* Add your JS files here, they will be combined in this order */
-      "js/vendor/jquery.min.js",
-      "js/vendor/jquery.easing.1.3.js",
-      "js/vendor/jquery.stellar.min.js",
-      "js/vendor/jquery.flexslider-min.js",
-      "js/vendor/imagesloaded.pkgd.min.js",
-      "js/vendor/isotope.pkgd.min.js",
-      "js/vendor/jquery.countTo.js",
-      "js/vendor/jquery.magnific-popup.min.js",
-      "js/vendor/photoswipe.min.js",
-      "js/vendor/photoswipe-ui-default.min.js",
-      "js/vendor/owl.carousel.min.js",
-      "js/vendor/bootstrap.min.js",
-      "js/vendor/jquery.waypoints.min.js",
-    ])
-    .pipe(concat("scripts.js"))
-    .pipe(gulp.dest("js"))
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(uglify())
-    .pipe(gulp.dest("js"));
-});
-
-gulp.task("minify-main", function () {
-  return gulp
-    .src([
-      /* Add your JS files here, they will be combined in this order */
-      "js/main.js",
-    ])
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(uglify())
-    .pipe(gulp.dest("js"));
-});
-
-// Sass task as a named function for Gulp 4.x compatibility
+/* ===============================
+   Sass Compilation Task
+================================= */
 function sassTask() {
   return gulp
-    .src("scss/style.scss")
+    .src("scss/style.scss", { allowEmpty: true })
     .pipe(plumber())
     .pipe(
-      sass({
+      gulpSass({
         errLogToConsole: true,
         outputStyle: "expanded",
         precision: 10,
-      })
+      }).on("error", gulpSass.logError)
     )
     .pipe(sourcemaps.init())
     .pipe(
       autoprefixer({
-        browsers: ["last 2 versions"],
+        overrideBrowserslist: ["last 2 versions"],
         cascade: false,
       })
     )
@@ -79,74 +38,90 @@ function sassTask() {
     .pipe(rename({ suffix: ".min" }))
     .pipe(minifycss())
     .pipe(gulp.dest("css"))
-    .pipe(reload({ stream: true }));
+    .pipe(browserSync.stream());
 }
-gulp.task("sass", sassTask);
 
+/* ===============================
+   JS Build Task
+================================= */
+gulp.task("scripts", function () {
+  return gulp
+    .src(
+      [
+        "js/vendor/jquery.min.js",
+        "js/vendor/jquery.easing.1.3.js",
+        "js/vendor/jquery.stellar.min.js",
+        "js/vendor/jquery.flexslider-min.js",
+        "js/vendor/imagesloaded.pkgd.min.js",
+        "js/vendor/isotope.pkgd.min.js",
+        "js/vendor/jquery.countTo.js",
+        "js/vendor/jquery.magnific-popup.min.js",
+        "js/vendor/photoswipe.min.js",
+        "js/vendor/photoswipe-ui-default.min.js",
+        "js/vendor/owl.carousel.min.js",
+        "js/vendor/bootstrap.min.js",
+        "js/vendor/jquery.waypoints.min.js",
+      ],
+      { allowEmpty: true }
+    )
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("js"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(uglify())
+    .pipe(gulp.dest("js"));
+});
+
+/* ===============================
+   Merge & Minify CSS Vendor Files
+================================= */
 gulp.task("merge-styles", function () {
-  return (
-    gulp
-      .src([
+  return gulp
+    .src(
+      [
         "css/vendor/bootstrap.min.css",
         "css/vendor/animate.css",
-        "css/vendor/icomoon.css",
+        "css/vendor/icomoon.css", // ← even if missing, won’t break build
         "css/vendor/flexslider.css",
         "css/vendor/owl.carousel.min.css",
         "css/vendor/owl.theme.default.min.css",
         "css/vendor/magnific-popup.css",
         "css/vendor/photoswipe.css",
         "css/vendor/default-skin.css",
-        "fonts/icomoon/style.css",
-      ])
-      // .pipe(sourcemaps.init())
-      // .pipe(autoprefixer({
-      //     browsers: ['last 2 versions'],
-      //     cascade: false
-      // }))
-      .pipe(concat("styles-merged.css"))
-      .pipe(gulp.dest("css"))
-      // .pipe(rename({suffix: '.min'}))
-      // .pipe(minifycss())
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("css"))
-      .pipe(reload({ stream: true }))
-  );
+      ],
+      { allowEmpty: true }
+    )
+    .pipe(concat("styles-merged.css"))
+    .pipe(gulp.dest("css"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(minifycss())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
 });
 
-/* Reload task */
-gulp.task("bs-reload", function () {
-  browserSync.reload();
-});
-
-/* Prepare Browser-sync for localhost */
+/* ===============================
+   BrowserSync Task
+================================= */
 gulp.task("browser-sync", function () {
-  browserSync.init(["css/*.css", "js/*.js"], {
-    proxy: "localhost/probootstrap/enlight",
-    /* For a static server you would use this: */
-    /*
-        server: {
-            baseDir: './'
-        }
-        */
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
   });
 });
 
-/* Watch scss, js and html files, doing different things with each. */
+/* ===============================
+   Watch Task
+================================= */
 function watchFiles() {
-  /* Watch scss, run the sass task on change. */
-  gulp.watch(["scss/*.scss", "scss/**/*.scss"], gulp.series(sassTask));
-  /* Watch app.js file, run the scripts task on change. */
-  gulp.watch(["js/main.js"], gulp.series("minify-main"));
-  /* Watch .html files, run the bs-reload task on change. */
-  gulp.watch(["*.html"], gulp.series("bs-reload"));
+  gulp.watch(["scss/**/*.scss"], gulp.series(sassTask));
+  gulp.watch(["js/**/*.js"], gulp.series("scripts"));
+  gulp.watch(["*.html"]).on("change", browserSync.reload);
 }
 
-gulp.task(
-  "default",
-  gulp.series(gulp.parallel(sassTask, "scripts", "browser-sync"), watchFiles)
-);
-// Production build task: runs all main asset tasks
-gulp.task(
-  "build",
-  gulp.series(sassTask, "scripts", "minify-main", "merge-styles")
-);
+/* ===============================
+   Combined Tasks
+================================= */
+gulp.task("default", gulp.series(gulp.parallel(sassTask, "scripts", "browser-sync"), watchFiles));
+
+gulp.task("build", gulp.series(sassTask, "scripts", "merge-styles"));
